7a007185558ac7b6a16921ed718db62ce3aaeb5c
chadlaban <laban.chad@gmail.com>
2024-10-30 00:30:59 +0800

    Add: Home page.

--- watch-it-frontend/src/pages/Home.js
+++ watch-it-frontend/src/pages/Home.js
@@ -1,12 +1,18 @@
 import React from "react";
+import NowPlaying from "../components/NowPlaying";
 // import { Link } from "react-router-dom";
 
 function Home() {
   return (
-    <article>
-      <h1>Watch It Home</h1>
-      <p>This is the Homepage.</p>
-    </article>
+    <>
+      <article>
+        <h1>Watch It Home</h1>
+        <p>This is the Homepage.</p>
+      </article>
+      <div>
+        <NowPlaying />
+      </div>
+    </>
   );
 }
 
