e430210b6ac1c91c6bea81fa21051bfd070b25a1
chadlaban <laban.chad@gmail.com>
2024-10-30 22:21:23 +0800

    Add: Search page.

--- watch-it-frontend/src/App.js
+++ watch-it-frontend/src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home.js";
+import Search from "./pages/Search.js";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />}></Route>
+      <Route path="/search" element={<Search />}></Route>
     </Routes>
   );
 }
