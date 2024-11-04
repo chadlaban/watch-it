4e79e6232c6165cd8e31ada500ae0feb5bad1a52
Chad <laban.chad@gmail.com>
2024-11-04 20:59:33 +0800

    Add: dynamic page for details and hook for id search.

---
+++ watch-it-backend/src/config/routes/searchById.js
@@ -0,0 +1,23 @@
+import "dotenv/config";
+
+const SearchById = async (id) => {
+  const url = `https://api.themoviedb.org/3/movie/${id}?language=en-US`;
+  const options = {
+    method: "GET",
+    headers: {
+      accept: "application/json",
+      Authorization: `Bearer ${process.env.AUTH_KEY}`,
+    },
+  };
+
+  try {
+    const res = await fetch(url, options);
+    const json = await res.json();
+    return json;
+  } catch (err) {
+    console.error(err);
+    throw new Error("Failed to fetch now playing movies.");
+  }
+};
+
+export default SearchById;
---
+++ watch-it-frontend/src/hooks/DetailsHook.js
@@ -0,0 +1,35 @@
+import { useState, useEffect } from "react";
+
+const useFetchById = (id) => {
+  const [data, setData] = useState(null);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    if (!id) return;
+
+    const fetchData = async () => {
+      setLoading(true);
+      try {
+        console.log(`${window.env.API_DETAILS_URL}${id}`);
+
+        const response = await fetch(`${window.env.API_DETAILS_URL}${id}`);
+        if (!response.ok) {
+          throw new Error("Failed to fetch data");
+        }
+        const result = await response.json();
+        setData(result);
+      } catch (err) {
+        setError(err.message);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, [id]);
+
+  return { data, error, loading };
+};
+
+export default useFetchById;
