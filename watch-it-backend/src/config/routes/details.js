672d49d4f32d3e211f5256db14c1e14a7de0ac95
Chad <laban.chad@gmail.com>
2024-11-09 23:11:26 +0800

    Update: file name and exported functions

---
+++ watch-it-backend/src/config/routes/details.js
@@ -0,0 +1,63 @@
+import "dotenv/config";
+
+const MovieDetails = async (id) => {
+  const url = `https://api.themoviedb.org/3/movie/${id}?language=en-US`;
+  const options = {
+    method: "GET",
+    headers: {
+      accept: "application/json",
+      Authorization: `Bearer ${process.env.AUTH_KEY}`,
+    },
+  };
+
+  try {
+    const res = await fetch(url, options);
+    const json = await res.json();
+    return json;
+  } catch (err) {
+    console.error(err);
+    throw new Error("Failed to fetch now playing movies.");
+  }
+};
+
+const Reviews = async (id) => {
+  const url = `https://api.themoviedb.org/3/movie/${id}/reviews`;
+  const options = {
+    method: "GET",
+    headers: {
+      accept: "application/json",
+      Authorization: `Bearer ${process.env.AUTH_KEY}`,
+    },
+  };
+
+  try {
+    const res = await fetch(url, options);
+    const json = await res.json();
+    return json;
+  } catch (err) {
+    console.error(err);
+    throw new Error("Failed to fetch now reviews on movie.");
+  }
+};
+
+const SimilarMovies = async (id) => {
+  const url = `https://api.themoviedb.org/3/movie/${id}/similar`;
+  const options = {
+    method: "GET",
+    headers: {
+      accept: "application/json",
+      Authorization: `Bearer ${process.env.AUTH_KEY}`,
+    },
+  };
+
+  try {
+    const res = await fetch(url, options);
+    const json = await res.json();
+    return json;
+  } catch (err) {
+    console.error(err);
+    throw new Error("Failed to fetch now reviews on movie.");
+  }
+};
+
+export { MovieDetails, Reviews, SimilarMovies };
