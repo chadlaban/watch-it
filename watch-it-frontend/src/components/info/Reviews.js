d4a95bd430ded63f2a7658e705c842e9da995662
Chad <laban.chad@gmail.com>
2024-11-13 00:05:40 +0800

    Update: avatar profile

--- watch-it-frontend/src/components/info/Reviews.js
+++ watch-it-frontend/src/components/info/Reviews.js
@@ -1,17 +1,35 @@
 import React from "react";
 import Paginate from "../pagination/Paginate.js";
 import StringLimit from "../string/StringLimit.js";
+import NoPostImage from "../../assets/imgs/placeholder/no-poster.jpg";
 
 const Reviews = ({ data }) => {
   const reviews = data.reviews.results || [];
 
   const renderReview = (review, index) => (
     <div key={index} className="border-b border-gray-200 p-4">
-      <h4 className="text-lg font-semibold text-gray-800">{review.author}</h4>
-      <p className="text-gray-600 text-sm mb-2">
-        {new Date(review.created_at).toLocaleDateString()}
-      </p>
-      <StringLimit review={review.content} limit={200} />
+      <div className="flex items-center gap-2">
+        <img
+          className="rounded-full w-12 h-12 object-cover border"
+          src={
+            review.author_details.avatar_path
+              ? `${process.env.REACT_APP_API_IMG_URL}${review.author_details.avatar_path}`
+              : NoPostImage
+          }
+          alt={review.author + "-profile"}
+        />
+        <article className="flex-col">
+          <h4 className="text-lg font-semibold text-gray-800">
+            {review.author}
+          </h4>
+          <p className="text-gray-600 text-sm mb-2">
+            {new Date(review.created_at).toLocaleDateString()}
+          </p>
+        </article>
+      </div>
+      <article>
+        <StringLimit review={review.content} limit={200} />
+      </article>
     </div>
   );
 
