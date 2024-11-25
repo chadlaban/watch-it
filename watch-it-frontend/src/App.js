b4c8f3a30f2ca41b857bef211d0cea0b00d79ff2
Chad <laban.chad@gmail.com>
2024-11-26 00:34:44 +0800

    Update: page for movies/series

--- watch-it-frontend/src/App.js
+++ watch-it-frontend/src/App.js
@@ -5,6 +5,7 @@ import Search from "./pages/Search.js";
 import Header from "./components/Header.js";
 import Footer from "./components/Footer.js";
 import { Details } from "./components/info/Details.js";
+import { SeeMore } from "./components/slideInPage/SeeMore.js";
 
 function App() {
   return (
@@ -14,8 +15,10 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />}></Route>
           <Route path="/search" element={<Search />}></Route>
+          <Route path="/movie" element={<SeeMore type="movie" />}></Route>
           <Route path="/movie/:id" element={<Details type="movie" />}></Route>
           <Route path="/series/:id" element={<Details type="series" />}></Route>
+          <Route path="/series" element={<SeeMore type="series" />}></Route>
           <Route path="/tv/:id" element={<Details type="tv" />}></Route>
           <Route path="/person/:id" element={<Details type="person" />}></Route>
         </Routes>
